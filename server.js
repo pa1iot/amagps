08419f71c8aea40ee13a302707ffc7e454d3ef0e
NighaTechGlobal <nighatechglobal@gmail.com>
2025-06-09 07:14:23 +0530

    vercel host mongodb

--- server.js
+++ server.js
@@ -6,7 +6,17 @@ const { MongoClient } = require('mongodb');
 const app = express();
 const PORT = 8000;
 
-const uri = 'mongodb+srv://nighatechglobal:yC2h3ooatd6lPKv2@24krafts.ocst4.mongodb.net/AmaGnss?retryWrites=true&w=majority';
+
+// local server
+// const uri = 'mongodb+srv://nighatechglobal:yC2h3ooatd6lPKv2@24krafts.ocst4.mongodb.net/AmaGnss?retryWrites=true&w=majority';
+
+
+// vercel host 
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("✅ Connected to MongoDB"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
+
+
 
 let collection;
 
