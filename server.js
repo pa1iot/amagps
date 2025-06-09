f6010eb7fc9a4e45dea00d8ada51d8863e7c50fa
NighaTechGlobal <nighatechglobal@gmail.com>
2025-06-09 07:24:42 +0530

    fix env pull

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const PORT = 8000;
 
 
 // vercel host 
+const MONGO_URI = process.env.MONGODB_URI;
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
